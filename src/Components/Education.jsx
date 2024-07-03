75cc9fbc04ad69e81594fa84bc78b2971a28d000
Hemlata Bainsla <162956405+Hemlata3010@users.noreply.github.com>
2024-07-03 08:57:25 +0000

    guy

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,10 +1,26 @@
+// education.jsx
 import React from "react";
+import "./education.css";
 
 const Education = () => {
   return (
-    <section className="light" id="education">
-      <h2>Education</h2>
-    </section>
+    <div className="education-container">
+      <h1 className="title">Education</h1>
+      <p className="description">This is the education page.</p>
+      <div className="education-list">
+        <h2>Academic Background</h2>
+        <ul>
+          <li>
+            <span>Master of Computer Application</span>
+            <span>DCRUST Sonipat, 2023-2025</span>
+          </li>
+          <li>
+            <span>Bachelor of Computer Application</span>
+            <span>MDU Rohtak, 2021-2023</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
